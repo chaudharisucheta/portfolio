6414fb1ad7b52ac3cbcff3c788567df7168d74a1
sucheta-codekul <sucheta.codekul@gmail.com>
2021-10-23 09:37:03 +0530

    removed contact

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from "./components/pages/home/Home";
 import NavMenu from "./components/navMenu/NavMenu";
 import Skills from "./components/pages/skills/Skills";
 import Projects from "./components/pages/projects/Projects";
-import Contact from "./components/pages/contact/Contact";
 import Tools from "./components/pages/tools/Tools";
 import Profile from "./components/pages/profile/Profile";
 
@@ -22,7 +21,6 @@ function App() {
 					<Route exact path="/" component={Home} />
 					<Route exact path="/skills" component={Skills} />
 					<Route exact path="/projects" component={Projects} />
-					<Route exact path="/contact" component={Contact} />
 					<Route exact path="/tools" component={Tools} />
 					<Route exact path="/profile" component={Profile} />
 				</Switch>
