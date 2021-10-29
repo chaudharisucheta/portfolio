29e35b9848d2121cb363dfc40dc93e913b6c8ffc
sucheta-codekul <sucheta.codekul@gmail.com>
2021-10-29 10:46:05 +0530

    added visit link

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -6,8 +6,11 @@ import {
 	Card,
 	CardContent,
 	CardMedia,
+	CardActions,
 	Typography,
+	Button
 } from "@material-ui/core";
+import { VisibilitySharp } from "@material-ui/icons";
 
 
 //-----> Image's & Icon's
@@ -26,24 +29,28 @@ const Projects = () => {
 			name: "Training Institute",
 			description:"Most suitable platform for students, professionals to keep themselves up-to-date with trending courses which are provided online as well as offline.Also suitable for corporates who want to train their staff on particular technology.",
 			image: traininginstitute,
+			url:'https://codejobs.in/'
 		},
 		{
 			name: "Omniscape",
 			description:
 				"Use the map tool to click on the locations you want and claim those using tokens.You can  claim virtual real estate ,view and create 3d content on the world map and travel to VR scenes that are linked to world map content.",
 			image: omniscape,
+			url:'https://omniscapexr-staging.web.app/'
 		},
 		{
 			name: "Bakekick",
 			description:
 				"Provides a platform for bakers to socialize and share their recipes or baking tools used by them.Also enable them to sell cakes,bread and other bakery items as well as the tools used for baking.",
 			image: bakekick,
+			url:'https://bakekick-5f0f6.web.app/#/'
 		},
 		{
 			name: "Review Matters",
 			description:
 				"Online reviews are more widespread than ever before, because they matter to consumers and to businesses.We provide a platform to connect, communicate and respond quickly to the customers.",
 			image: reviewmatters,
+			url:'https://reviewmatters.com/'
 		},
 		
 	];
@@ -56,7 +63,7 @@ const Projects = () => {
 					
 					<Grid container className="projects">
 						{webProjects.map(
-							({ image, name, description, source }) => {
+							({ image, name, description, url }) => {
 								return (
 									<Grid
 										item
@@ -66,6 +73,7 @@ const Projects = () => {
 										lg={3}
 										className="project"
 									>
+										
 										<Card
 											className="card"
 											data-aos="zoom-in"
@@ -92,8 +100,8 @@ const Projects = () => {
 													{description}
 												</Typography>
 											</CardContent>
-											{/* <CardActions className="card_buttons">
-												<Button
+											<CardActions className="card_buttons">
+												{/* <Button
 													variant="contained"
 													size="small"
 													endIcon={<GitHub />}
@@ -104,7 +112,7 @@ const Projects = () => {
 													}}
 												>
 													Source
-												</Button>
+												</Button> */}
 												<Button
 													variant="contained"
 													size="small"
@@ -115,11 +123,13 @@ const Projects = () => {
 														backgroundColor:
 															"#cfcfcf",
 														fontWeight: "600",
+														width:'100%'
 													}}
+													onClick={()=>window.open(url)}
 												>
 													Visit
 												</Button>
-											</CardActions> */}
+											</CardActions>
 										</Card>
 									</Grid>
 								);
